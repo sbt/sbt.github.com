1b520094fad5530d241dd17b85f418ef5af0beca
softprops <d.tangren@gmail.com>
2011-10-29 00:05:02 -0400

    temp using my fork for master branch support

--- project/plugins/project/build.scala
+++ project/plugins/project/build.scala
@@ -2,5 +2,7 @@ import sbt._
 object PluginDef extends Build {
   override def projects = Seq(root)
   lazy val root = Project("plugins", file(".")) dependsOn(ghpages)
-  lazy val ghpages = uri("git://github.com/jsuereth/xsbt-ghpages-plugin.git")
+  // move back to josh's repo after pull requests for git branch key
+  // support goes through
+  lazy val ghpages = uri("git://github.com/softprops/xsbt-ghpages-plugin.git#pikka-branch")
 }
